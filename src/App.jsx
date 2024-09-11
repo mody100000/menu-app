01fe21b8a3b9c3dacdbfb9363afff5e665477fed
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2024-09-11 15:57:52 +0300

    adding ScrollToTop function

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,39 @@
 import './App.css';
 import MenuPage from './Pages/MenuPage';
 import { ParallaxProvider } from 'react-scroll-parallax';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import AboutUsPage from './Pages/AboutUsPage';
 import ContactUsPage from './Pages/ContactUsPage';
 import HomePage from './Pages/HomePage';
+import ScrollToTop from './Utils/ScrollToTop';
+
+// Create a layout component that includes ScrollToTop
+const Layout = () => (
+  <>
+    <ScrollToTop />
+    <Outlet />
+    {/* Other components like <Navbar /> can go here */}
+  </>
+);
+
+// Define routes
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/menu", element: <MenuPage /> },
+      { path: "/about-us", element: <AboutUsPage /> },
+      { path: "/contact-us", element: <ContactUsPage /> },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <Router>
-      <ParallaxProvider>
-        {/* <Navbar />  */}
-        <Routes>
-          {/* Define your routes here */}
-          <Route path="/" element={<HomePage />} />
-          <Route path="/menu" element={<MenuPage />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/contact-us" element={<ContactUsPage />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </ParallaxProvider>
-    </Router>
+    <ParallaxProvider>
+      <RouterProvider router={router} />
+    </ParallaxProvider>
   );
 }
 
